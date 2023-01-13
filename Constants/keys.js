541afe142a3ae65dafff53f5f0bcf40adc3b4af0
abhishek1342 <119cs0044@iiitk.ac.in>
2023-01-14 05:16:25 +0530

    added api and also called

---
+++ Constants/keys.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        "X-RapidAPI-Key": "3dcf5d7931msh8dd32ea41c3357fp192eaejsn99c212b2dfa8",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "ec024eb781msh6ddf7d7b6e847e1p177749jsn56cc0a2f9572",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "3f327e553fmsha03198a8749840bp1f2951jsn84f48d362f9c",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "bcf17279edmsh0eba17b156f160cp1f4e51jsn5962829d2cc2",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "0ff92e1fd5msh230a768b5305795p156052jsn3e39f6097e46",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "8487704fd9mshebf130842123a3ep168e10jsn714b3b4e6086",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "f838169380msh792cef846082f8cp1175b7jsndfb80ecbc3a6",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "d101f870b7msh1cd48233a0c952dp1a8240jsn0c15509897c4",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "ddccf5d7b2mshb92e0cfa03305ecp193dc6jsn981291a51c6f",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+    {
+        "X-RapidAPI-Key": "2871145f72mshf6554abeff2a63dp10be73jsn3fbebf6eb409",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+    },
+];
--- Routes/search.routes.js
+++ Routes/search.routes.js
@@ -1,65 +1,71 @@
-const { default: axios } = require('axios')
-const express = require('express')
-const router = express.Router()
-const { db } = require('../firebase')
+const { default: axios } = require("axios");
+const express = require("express");
+const router = express.Router();
+const { db } = require("../firebase");
+const apiKey = require("../Constants/keys");
 
-router.get('/fetch_keyword_videos', async (req, res, next) => {
-  try {
-    const keyword = req.query.keyword
-    const forEmail = req.query.forEmail
+router.get("/fetch_keyword_videos", async (req, res, next) => {
+    try {
+        const keyword = req.query.keyword;
+        const forEmail = req.query.forEmail;
 
-    let FETCH_COUNT = 0
-    let hasNext = true
-    let cursor = '0'
+        let FETCH_COUNT = 0;
+        let apiUseCount = 0;
+        let hasNext = true;
+        let cursor = "0";
+        const getApiKey = () => {
+            return apiKey[apiUseCount % 10];
+        };
 
-    let options = {
-      method: 'GET',
-      url: 'https://tiktok-video-no-watermark2.p.rapidapi.com/feed/search',
-      params: { keywords: keyword, count: '30', cursor },
-      headers: {
-        'X-RapidAPI-Key': 'ec024eb781msh6ddf7d7b6e847e1p177749jsn56cc0a2f9572',
-        'X-RapidAPI-Host': 'tiktok-video-no-watermark2.p.rapidapi.com'
-      }
-    }
+        let options = {
+            method: "GET",
+            url: "https://tiktok-video-no-watermark2.p.rapidapi.com/feed/search",
+            params: { keywords: keyword, count: "30", cursor },
+            headers: getApiKey(),
+        };
 
-    while (FETCH_COUNT < 30 && hasNext) {
-      const response = await axios.request(options)
+        while (FETCH_COUNT < 30 && hasNext) {
+            const response = await axios.request(options);
 
-      // console.log(response.data.data.videos)
-      hasNext = response.data.data.hasMore
-      cursor = response.data.data.cursor
-      options = { ...options, params: { ...options.params, cursor } }
-      response.data.data.videos.forEach(async video => {
-        const vidRef = db.collection('videos').doc(video.video_id)
-        const vid = await vidRef.get()
-        if (!vid.exists) {
-          await db
-            .collection('videos')
-            .doc(video.video_id)
-            .set({
-              ...video,
-              keyword,
-              forEmail,
-              title: video.title.substr(0, 75),
-              description: video.title,
-              tags: video.title.split('#').join(', #').substr(0, 500),
-              uploaded: false
-            })
+            // console.log(response.data.data.videos)
+            hasNext = response.data.data.hasMore;
+            cursor = response.data.data.cursor;
+            options = { ...options, params: { ...options.params, cursor } };
+            response.data.data.videos.forEach(async (video) => {
+                const vidRef = db.collection("videos").doc(video.video_id);
+                const vid = await vidRef.get();
+                if (!vid.exists) {
+                    await db
+                        .collection("videos")
+                        .doc(video.video_id)
+                        .set({
+                            ...video,
+                            keyword,
+                            forEmail,
+                            title: video.title.substr(0, 75),
+                            description: video.title,
+                            tags: video.title
+                                .split("#")
+                                .join(", #")
+                                .substr(0, 500),
+                            uploaded: false,
+                        });
 
-          FETCH_COUNT += 1
-        } else {
-          console.log('Document already exists!')
+                    FETCH_COUNT += 1;
+                    apiUseCount += 1;
+                } else {
+                    console.log("Document already exists!");
+                }
+            });
+
+            console.log(options);
         }
-      })
-
-      console.log(options)
+        res.status(200).json({ msg: "ok", FETCH_COUNT });
+    } catch (error) {
+        if (!error.statusCode) error.statusCode = 500;
+        console.log(error);
+        return next(error);
     }
-    res.status(200).json({ msg: 'ok', FETCH_COUNT })
-  } catch (error) {
-    if (!error.statusCode) error.statusCode = 500
-    console.log(error)
-    return next(error)
-  }
-})
+});
 
-module.exports = router
+module.exports = router;
