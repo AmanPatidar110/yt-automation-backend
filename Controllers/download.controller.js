f4d83d400ee993b518390b868606a8020a5b4335
abhishek1342 <119cs0044@iiitk.ac.in>
2023-01-15 01:33:27 +0530

    muted audio and used sstik

--- Controllers/download.controller.js
+++ Controllers/download.controller.js
@@ -1,42 +1,40 @@
-const https = require('https')
-const fs = require('fs')
+const https = require("https");
+const fs = require("fs");
+const download = require("download");
+const Ffmpeg = require("fluent-ffmpeg");
 
-exports.fileDownload = (url, dest) => {
-  const file = fs.createWriteStream(dest)
-  return new Promise((resolve, reject) => {
-    let responseSent = false // flag to make sure that response is sent only once.
-    https
-      .get(url, response => {
-        response.pipe(file)
-        file.on('finish', () => {
-          file.close(() => {
-            if (responseSent) return
-            responseSent = true
-            resolve()
-          })
-        })
-      })
-      .on('error', err => {
-        if (responseSent) return
-        responseSent = true
-        reject(err)
-      })
-  })
-}
+exports.fileDownloadWithoutAudio = async (url, videoId) => {
+    return new Promise((resolve, reject) => {
+        console.log("Downloading...");
+        Ffmpeg(
+            download(url)
+                .on("end", () => {
+                    console.log("Downloaded");
+                    resolve();
+                })
+                .on("error", (err) => {
+                    console.log(err);
+                    reject();
+                })
+        )
+            .withNoAudio()
+            .saveToFile(`./Videos/${videoId}.mp4`);
+    });
+};
 
-exports.removeFile = dest => {
-  return new Promise(resolve => {
-    fs.unlink(dest, function (err) {
-      if (err && err.code === 'ENOENT') {
-        // file doens't exist
-        console.info("File doesn't exist, won't remove it.")
-      } else if (err) {
-        // other errors, e.g. maybe we don't have enough permission
-        console.error('Error occurred while trying to remove file')
-      } else {
-        console.info('removed')
-        resolve()
-      }
-    })
-  })
-}
+exports.removeFile = (dest) => {
+    return new Promise((resolve) => {
+        fs.unlink(dest, function (err) {
+            if (err && err.code === "ENOENT") {
+                // file doens't exist
+                console.info("File doesn't exist, won't remove it.");
+            } else if (err) {
+                // other errors, e.g. maybe we don't have enough permission
+                console.error("Error occurred while trying to remove file");
+            } else {
+                console.info("removed");
+                resolve();
+            }
+        });
+    });
+};
