5c785cddeab1cfb173a6243b83eef5f0c6c73904
abhishek1342 <119cs0044@iiitk.ac.in>
2023-01-15 22:26:44 +0530

    working on new uploader

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const http = require('http')
-var cors = require('cors')
+const express = require("express");
+const http = require("http");
+var cors = require("cors");
 
-const searchRoutes = require('./Routes/search.routes')
-const uploadRoutes = require('./Routes/upload.routes')
+const searchRoutes = require("./Routes/search.routes");
+const uploadRoutes = require("./Routes/upload.routes");
 // const cheerio = require('cheerio')
 
-const app = express()
-app.use(cors())
-const server = http.createServer(app)
-const PORT = process.env.PORT || 4000
+const app = express();
+app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log('listening on port *: ', PORT)
-})
+    console.log("listening on port *: ", PORT);
+});
 
-app.use('/search', searchRoutes)
-app.use('/upload', uploadRoutes)
+app.use("/search", searchRoutes);
+app.use("/upload", uploadRoutes);
 
 app.use((error, req, res, next) => {
-  if (!error.statusCode) error.statusCode = 500
-  if (!error.message) error.message = 'Server side error'
-  const status = error.statusCode
-  const message = error.message
-  const data = error.data
+    if (!error.statusCode) error.statusCode = 500;
+    if (!error.message) error.message = "Server side error";
+    const status = error.statusCode;
+    const message = error.message;
+    const data = error.data;
 
-  res.status(status).json({ message: message, data: data })
-})
+    res.status(status).json({ message: message, data: data });
+});
 
 // let html = await axios.get(`https://api.vevioz.com/api/button/mp3/${vidID}`);
 
