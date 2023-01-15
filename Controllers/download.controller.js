9ac8a2156bff246b0c29930c3c46557ecb560283
Aman Patidar <63087129+AmanPatidar110@users.noreply.github.com>
2023-01-15 19:27:46 +0530

    formating

--- Controllers/download.controller.js
+++ Controllers/download.controller.js
@@ -1,40 +1,39 @@
-const https = require("https");
-const fs = require("fs");
-const download = require("download");
-const Ffmpeg = require("fluent-ffmpeg");
+const fs = require('fs')
+const download = require('download')
+const Ffmpeg = require('fluent-ffmpeg')
 
 exports.fileDownloadWithoutAudio = async (url, videoId) => {
-    return new Promise((resolve, reject) => {
-        console.log("Downloading...");
-        Ffmpeg(
-            download(url)
-                .on("end", () => {
-                    console.log("Downloaded");
-                    resolve();
-                })
-                .on("error", (err) => {
-                    console.log(err);
-                    reject();
-                })
-        )
-            .withNoAudio()
-            .saveToFile(`./Videos/${videoId}.mp4`);
-    });
-};
+  return new Promise((resolve, reject) => {
+    console.log('Downloading...')
+    Ffmpeg(
+      download(url)
+        .on('end', () => {
+          console.log('Downloaded')
+          resolve()
+        })
+        .on('error', err => {
+          console.log(err)
+          reject(err)
+        })
+    )
+      .withNoAudio()
+      .saveToFile(`./Videos/${videoId}.mp4`)
+  })
+}
 
-exports.removeFile = (dest) => {
-    return new Promise((resolve) => {
-        fs.unlink(dest, function (err) {
-            if (err && err.code === "ENOENT") {
-                // file doens't exist
-                console.info("File doesn't exist, won't remove it.");
-            } else if (err) {
-                // other errors, e.g. maybe we don't have enough permission
-                console.error("Error occurred while trying to remove file");
-            } else {
-                console.info("removed");
-                resolve();
-            }
-        });
-    });
-};
+exports.removeFile = dest => {
+  return new Promise(resolve => {
+    fs.unlink(dest, function (err) {
+      if (err && err.code === 'ENOENT') {
+        // file doens't exist
+        console.info("File doesn't exist, won't remove it.")
+      } else if (err) {
+        // other errors, e.g. maybe we don't have enough permission
+        console.error('Error occurred while trying to remove file')
+      } else {
+        console.info('removed')
+        resolve()
+      }
+    })
+  })
+}
--- Routes/upload.routes.js
+++ Routes/upload.routes.js
@@ -1,87 +1,86 @@
-const express = require("express");
-const router = express.Router();
-const ffmpeg = require("fluent-ffmpeg");
-const cheerio = require("cheerio");
+const express = require('express')
+const router = express.Router()
+const cheerio = require('cheerio')
 
-const { YoutubeUploader } = require("@luthfipun/yt-uploader");
+const { YoutubeUploader } = require('@luthfipun/yt-uploader')
 const {
-    UPLOAD_PRIVACY_PRIVATE,
-} = require("@luthfipun/yt-uploader/dist/helpers/youtubeUploaderOptions");
-const { db } = require("../firebase");
+  UPLOAD_PRIVACY_PRIVATE
+} = require('@luthfipun/yt-uploader/dist/helpers/youtubeUploaderOptions')
+const { db } = require('../firebase')
 const {
-    fileDownloadWithoutAudio,
-    removeFile,
-} = require("../Controllers/download.controller");
+  fileDownloadWithoutAudio,
+  removeFile
+} = require('../Controllers/download.controller')
 const {
-    getVideoFromTiktokVideoId,
-} = require("../Controllers/getVIdeoFromTiktokVideoId");
+  getVideoFromTiktokVideoId
+} = require('../Controllers/getVIdeoFromTiktokVideoId')
 
-router.get("/", async (req, res, next) => {
-    try {
-        let UPLOAD_COUNT = 0;
-        const email = req.query.email;
-        const password = req.query.password;
-        const targetUploadCount = req.query.targetUploadCount;
+router.get('/', async (req, res, next) => {
+  try {
+    let UPLOAD_COUNT = 0
+    const email = req.query.email
+    const password = req.query.password
+    const targetUploadCount = req.query.targetUploadCount
 
-        const videosRef = db.collection("videos");
-        const snapshot = await videosRef
-            .where("forEmail", "==", email)
-            .where("uploaded", "==", false)
-            .get();
-        console.log("snapshot", snapshot);
-        if (snapshot.empty) {
-            console.log("No matching videos.");
-            res.status(200).json({ msg: "No matching videos.", UPLOAD_COUNT });
-        }
-
-        const youtubeUploader = new YoutubeUploader(
-            "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-            UPLOAD_PRIVACY_PRIVATE
-        );
-
-        const videos = [];
-        snapshot.forEach((vid) => {
-            videos.push(vid.data());
-        });
-
-        for (const video of videos) {
-            console.log(video.video_id, "=>", UPLOAD_COUNT, targetUploadCount);
-            if (UPLOAD_COUNT >= targetUploadCount) {
-                break;
-            }
-            await youtubeUploader.Login(email, password);
-            console.log("LoggedIn to your account and muting audio");
-            const $ = cheerio.load(
-                await getVideoFromTiktokVideoId(video.video_id, video.unique_id)
-            );
-            const videoURL = $("a").first().attr("href");
-            console.log(videoURL);
-
-            await fileDownloadWithoutAudio(videoURL, video.video_id);
-            console.log("muting done. Now, Uploading ...");
-            await youtubeUploader.UploadVideo(
-                `Videos/${video.video_id}.mp4`,
-                video.title,
-                video.description,
-                "",
-                video.tags
-            );
-
-            UPLOAD_COUNT += 1;
-            await db.collection("videos").doc(video.video_id).update({
-                uploaded: true,
-            });
-
-            await removeFile(`Videos/${video.video_id}.mp4`);
-
-            console.log("Uploading successfully");
-        }
-
-        await youtubeUploader.CloseBrowser();
-        res.status(200).json({ msg: "Videos uploaded", UPLOAD_COUNT });
-    } catch (e) {
-        console.log(e);
+    const videosRef = db.collection('videos')
+    const snapshot = await videosRef
+      .where('forEmail', '==', email)
+      .where('uploaded', '==', false)
+      .get()
+    console.log('snapshot', snapshot)
+    if (snapshot.empty) {
+      console.log('No matching videos.')
+      res.status(200).json({ msg: 'No matching videos.', UPLOAD_COUNT })
     }
-});
 
-module.exports = router;
+    const youtubeUploader = new YoutubeUploader(
+      'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      UPLOAD_PRIVACY_PRIVATE
+    )
+
+    const videos = []
+    snapshot.forEach(vid => {
+      videos.push(vid.data())
+    })
+
+    for (const video of videos) {
+      console.log(video.video_id, '=>', UPLOAD_COUNT, targetUploadCount)
+      if (UPLOAD_COUNT >= targetUploadCount) {
+        break
+      }
+      await youtubeUploader.Login(email, password)
+      console.log('LoggedIn to your account and muting audio')
+      const $ = cheerio.load(
+        await getVideoFromTiktokVideoId(video.video_id, video.unique_id)
+      )
+      const videoURL = $('a').first().attr('href')
+      console.log(videoURL)
+
+      await fileDownloadWithoutAudio(videoURL, video.video_id)
+      console.log('muting done. Now, Uploading ...')
+      await youtubeUploader.UploadVideo(
+        `Videos/${video.video_id}.mp4`,
+        video.title,
+        video.description,
+        '',
+        video.tags
+      )
+
+      UPLOAD_COUNT += 1
+      await db.collection('videos').doc(video.video_id).update({
+        uploaded: true
+      })
+
+      await removeFile(`Videos/${video.video_id}.mp4`)
+
+      console.log('Uploading successfully')
+    }
+
+    await youtubeUploader.CloseBrowser()
+    res.status(200).json({ msg: 'Videos uploaded', UPLOAD_COUNT })
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+module.exports = router
