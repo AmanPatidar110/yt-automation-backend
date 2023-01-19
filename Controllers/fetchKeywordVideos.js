54bbab982da71365429c51a3148663455c3dac2f
Aman Patidar <63087129+AmanPatidar110@users.noreply.github.com>
2023-01-19 17:04:09 +0530

    api count logic fixed

--- Controllers/fetchKeywordVideos.js
+++ Controllers/fetchKeywordVideos.js
@@ -1,25 +1,35 @@
 const { default: axios } = require('axios')
 const apiKey = require('../Constants/keys')
+const { db } = require('../firebase')
 const { uploadVideosOnFirestore } = require('./fireStoreUpload.controller')
 
 const getApiKey = FETCH_COUNT => {
   return apiKey[FETCH_COUNT % 10]
 }
+const updateApiCount = async currentCount => {
+  const constantsRef = db.collection('constants').doc('Up8smqy8uZukjhDBCus9')
+  constantsRef.update({ api_count: currentCount + 1 })
+  return currentCount + 1
+}
 
 exports.fetchKeywordVideos = async (forEmail, keywords, forUser) => {
   const keyword = keywords[Math.floor(Math.random() * keywords.length)]
-  try {
-    let FETCH_COUNT = 0
-    let hasNext = true
-    let cursor = '0'
+  const constantsRef = db.collection('constants').doc('Up8smqy8uZukjhDBCus9')
 
+  let API_COUNT = 0
+  let FETCH_COUNT = 0
+  let hasNext = true
+  let cursor = '0'
+
+  try {
+    const constants = (await constantsRef.get()).data()
+    API_COUNT = constants.api_count
     let options = {
       method: 'GET',
       url: 'https://tiktok-video-no-watermark2.p.rapidapi.com/feed/search',
       params: { keywords: keyword, count: '30', cursor },
-      headers: getApiKey(FETCH_COUNT)
+      headers: getApiKey(API_COUNT)
     }
-
     while (FETCH_COUNT < 30 && hasNext) {
       const response = await axios.request(options)
 
@@ -40,10 +50,11 @@ exports.fetchKeywordVideos = async (forEmail, keywords, forUser) => {
         forUser,
         keywords
       )
+      API_COUNT = await updateApiCount(API_COUNT)
     }
   } catch (error) {
     if (!error.statusCode) error.statusCode = 500
     console.log(error)
-    throw error
+    API_COUNT = await updateApiCount(API_COUNT)
   }
 }
