3a1687f8fc4445cdb1fae179f0b873d76e24e2d5
Aman Patidar <63087129+AmanPatidar110@users.noreply.github.com>
2023-01-21 00:40:17 +0530

    pup build

--- Utility/api-service.js
+++ Utility/api-service.js
@@ -1,3 +1,3 @@
 export const apiServiceUrl =
   process.env.API_SERVICE_URL ||
-  'https://yt-automation-backend-3g63fn1k6-amanpatidar110.vercel.app'
+  'https://yt-automation-backend-m9xf8kgsy-amanpatidar110.vercel.app'
