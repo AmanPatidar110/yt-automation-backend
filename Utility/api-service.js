c0641492b69d049dc28b51693cd1c55735f05cdb
Aman Patidar <63087129+AmanPatidar110@users.noreply.github.com>
2023-01-21 02:13:48 +0530

    api service url

--- Utility/api-service.js
+++ Utility/api-service.js
@@ -1,3 +1,2 @@
 export const apiServiceUrl =
-  process.env.API_SERVICE_URL ||
-  'https://yt-automation-backend-m9xf8kgsy-amanpatidar110.vercel.app'
+  process.env.API_SERVICE_URL || 'https://api-service-theta.vercel.app'
