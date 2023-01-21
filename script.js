73a9b0daa6ed8ffe781408dc015ade5b7712f1d6
Aman Patidar <63087129+AmanPatidar110@users.noreply.github.com>
2023-01-22 03:51:39 +0530

    minor

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import * as dotenv from 'dotenv';
 
 import uploadRoutes from './Routes/upload.routes.js';
 import instaRoutes from './Routes/insta.routes.js';
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
