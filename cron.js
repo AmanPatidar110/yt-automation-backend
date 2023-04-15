1e41ebabf31baedb2db26539fd262e7015c6e54a
Aman <aamanpatidar110@gmail.com>
2023-04-15 20:13:12 +0530

    insta super machines cron removed

--- cron.js
+++ cron.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import cron from "node-cron";
+import axios from 'axios';
+import cron from 'node-cron';
 
 export const runCron = () => {
-  console.log("Scheduling crons");
-  cron.schedule("2 0 15,11,13,16,17 * * * *", async () => {
-    console.log("Hitting /upload for:", "urbanlibaaz@gmail.com");
+  console.log('Scheduling crons');
+  cron.schedule('2 0 15,11,13,16,17 * * * *', async () => {
+    console.log('Hitting /upload for:', 'urbanlibaaz@gmail.com');
 
     var config = {
-      method: "get",
+      method: 'get',
       url: `http://localhost:8999/upload?email=urbanlibaaz@gmail.com&forUser=AMAN&targetUploadCount=1`,
     };
 
@@ -15,11 +15,11 @@ export const runCron = () => {
     console.log(response.data);
   });
 
-  cron.schedule("2 12 15,11,13,16,17 * * * *", async () => {
-    console.log("Hitting /upload for:", "amanpatidar110@gmail.com");
+  cron.schedule('2 12 15,11,13,16,17 * * * *', async () => {
+    console.log('Hitting /upload for:', 'amanpatidar110@gmail.com');
 
     var config = {
-      method: "get",
+      method: 'get',
       url: `http://localhost:8999/upload?email=amanpatidar110@gmail.com&forUser=AMAN&targetUploadCount=1`,
     };
 
@@ -27,11 +27,11 @@ export const runCron = () => {
     console.log(response.data);
   });
 
-  cron.schedule("2 24 15,11,13,16,17 * * * *", async () => {
-    console.log("Hitting /upload for:", "aamanpatidar110@gmail.com");
+  cron.schedule('2 24 15,11,13,16,17 * * * *', async () => {
+    console.log('Hitting /upload for:', 'aamanpatidar110@gmail.com');
 
     var config = {
-      method: "get",
+      method: 'get',
       url: `http://localhost:8999/upload?email=aamanpatidar110@gmail.com&forUser=AMAN&targetUploadCount=1`,
     };
 
@@ -39,11 +39,11 @@ export const runCron = () => {
     console.log(response.data);
   });
 
-  cron.schedule("8 48 15,11,13,16,17 * * * *", async () => {
-    console.log("Hitting /upload for:", "akkupati0330@gmail.com");
+  cron.schedule('8 48 15,11,13,16,17 * * * *', async () => {
+    console.log('Hitting /upload for:', 'akkupati0330@gmail.com');
 
     var config = {
-      method: "get",
+      method: 'get',
       url: `http://localhost:8999/upload?email=akkupati0330@gmail.com&forUser=AMAN&targetUploadCount=1`,
     };
 
@@ -51,11 +51,11 @@ export const runCron = () => {
     console.log(response.data);
   });
 
-  cron.schedule("10 30 15,11,13,16,17 * * * *", async () => {
-    console.log("Hitting /upload for:", "galactica.shorts@gmail.com");
+  cron.schedule('10 30 15,11,13,16,17 * * * *', async () => {
+    console.log('Hitting /upload for:', 'galactica.shorts@gmail.com');
 
     var config = {
-      method: "get",
+      method: 'get',
       url: `http://localhost:8999/upload?email=galactica.shorts@gmail.com&forUser=AMAN&targetUploadCount=1`,
     };
 
@@ -63,39 +63,19 @@ export const runCron = () => {
     console.log(response.data);
   });
 
-  cron.schedule("0 1 * * *", async () => {
+  cron.schedule('0 1 * * *', async () => {
     console.log(
-      "Hitting /fetch_insta_messages for:",
-      "amanpatidar110@gmail.com"
+      'Hitting /fetch_insta_messages for:',
+      'amanpatidar110@gmail.com'
     );
 
     var config = {
-      method: "POST",
+      method: 'POST',
       url: `http://localhost:8999/insta/fetch_messages_from_insta`,
       data: {
-        forUser: "AMAN",
-        instaCredId: "amanpatidar110@gmail.com",
-        forChannelEmail: "amanpatidar110@gmail.com",
-      },
-    };
-
-    const response = await axios(config);
-    console.log(response.data);
-  });
-
-  cron.schedule("0 18 * * *", async () => {
-    console.log(
-      "Hitting /fetch_insta_messages for:",
-      "aamanpatidar110@gmail.com"
-    );
-
-    var config = {
-      method: "POST",
-      url: `http://localhost:8999/insta/fetch_messages_from_insta`,
-      data: {
-        forUser: "AMAN",
-        instaCredId: "super_machines_0",
-        forChannelEmail: "aamanpatidar110@gmail.com",
+        forUser: 'AMAN',
+        instaCredId: 'amanpatidar110@gmail.com',
+        forChannelEmail: 'amanpatidar110@gmail.com',
       },
     };
 
